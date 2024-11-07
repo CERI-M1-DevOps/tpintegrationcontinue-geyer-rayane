1cb59378227b8b955e7166b6d6b0944b51d1cdd7
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-07 15:09:55 +0100

    Add (

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -98,7 +98,7 @@ class ListeSimpleTest {
         listeATester.ajout(3);
         listeATester.ajout(4);
         listeATester.supprimePremier(2);
-        assertEquals"ListeSimple(Noeud(4), Noeud(3), Noeud(1))",listeATester.toString());
+        assertEquals("ListeSimple(Noeud(4), Noeud(3), Noeud(1))",listeATester.toString());
         assertEquals(3, listeATester.getSize());
     }
 
